1a55260685aee2c780ea79d152060a046ef7bb48
Smaran Shaha <smaranshaha@gmail.com>
2025-01-06 16:50:30 +0600

    now user authentication are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Inbox from "./pages/Inbox";
 import Conversation from "./pages/Conversation";
+import useAuthCheck from "./hooks/useAuthCheck";
 
 function App() {
-  return (
+  const authChecked = useAuthCheck();
+
+  return !authChecked ? (
+    <div>Checking authentication...</div>
+  ) : (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
