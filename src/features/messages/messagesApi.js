0396ae484275a258af8e98eb49d8ec3a30f8d610
Smaran Shaha <smaranshaha@gmail.com>
2025-01-08 13:28:53 +0600

    now specfic user can see his chat daynamicly and when he goes his chat history his send messages are on to right side

--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -1,7 +1,14 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const messagesApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => {
-    // endpoints here
-  },
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: (id) =>
+        `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${
+          import.meta.env.VITE_APP_MESSAGES_PER_PAGE
+        }`,
+    }),
+  }),
 });
+
+export const { useGetMessagesQuery } = messagesApi;
