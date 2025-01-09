2e4fde55cfda3da459a12aab2f529f317059213d
Smaran Shaha <smaranshaha@gmail.com>
2025-01-09 17:03:33 +0600

    now user also sent sms from the chat box ui

--- src/components/inbox/chatbody/Options.jsx
+++ src/components/inbox/chatbody/Options.jsx
@@ -1,12 +1,48 @@
-export default function Options() {
+import { useEffect, useState } from "react";
+import { useEditConversationMutation } from "../../../features/conversations/conversationsApi";
+import { useSelector } from "react-redux";
+
+export default function Options({ info }) {
+  const [message, setMessage] = useState("");
+  const [editConversation, { isSuccess }] = useEditConversationMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      setMessage("");
+    }
+  }, [isSuccess]);
+
+  const { user: loggedInUser } = useSelector((state) => state.auth);
+
+  const participantUser =
+    info.receiver.email !== loggedInUser.email ? info.receiver : info.sender;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editConversation({
+      id: info?.conversationId,
+      sender: loggedInUser?.email,
+      data: {
+        participants: `${loggedInUser.email}-${participantUser.email}`,
+        users: [loggedInUser, participantUser],
+        message,
+        timestamp: new Date().getTime(),
+      },
+    });
+  };
   return (
-    <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center justify-between w-full p-3 border-t border-gray-300"
+    >
       <input
         type="text"
         placeholder="Message"
         className="block w-full py-2 pl-4 mx-3 bg-gray-100 focus:ring focus:ring-violet-500 rounded-full outline-none focus:text-gray-700"
         name="message"
         required
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       />
       <button type="submit">
         <svg
@@ -18,6 +54,6 @@ export default function Options() {
           <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
         </svg>
       </button>
-    </div>
+    </form>
   );
 }
